01ce1a8cae83cc78c20c561f5abdfdce1bb12104
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-03-18 11:54:08 -0500

    Account for vaccinations in simulated infections

--- R/simulate_infections.R
+++ R/simulate_infections.R
@@ -1,28 +1,28 @@
-simulate_infections <- function(rt, h = 1L, population = 1e6, detection = 1) {
+simulate_infections <- function(rt, vac_init, vac_per_day = 3000, vac_init_pct = 0.25, vac_add_pct = vac_init_pct, h = 1L, population = 1e6, detection = 1/3) {
 
+  # Adjust incidence for detection rate
   rt <- dplyr::mutate(rt, .incid = .incid / detection)
 
+  # Get parameters up to latest time point
   .t <- rt[[".t"]]
-
   incid <- rt[[".incid"]]
-
   cumulative_incid <- incid %>% tidyr::replace_na(0) %>% cumsum()
-
   active <- estimate_activity(rt)
 
+  # Get starting values for simulation
   input_size <- vec_size(rt)
-
   next_incid_diff <- incid[[input_size]] - incid[[input_size - 1L]]
-
   next_incid <- incid[[input_size]] + next_incid_diff
 
+  # Parameterize rt and serial interval
   current_rt <- rt[[".mean"]][[input_size]]
-
   serial_interval_params <- attr(rt, "serial_interval")
 
-  initial_susceptible_pct <- population %>%
+  # Get initial susceptible percentage
+  initial_susceptible <- population %>%
     subtract(cumulative_incid[[vec_size(cumulative_incid)]]) %>%
-    divide_by(population)
+    subtract(vac_init * vac_init_pct)
+  initial_susceptible_pct <- initial_susceptible / population
 
   for (t in seq_len(h)) {
 
@@ -30,7 +30,11 @@ simulate_infections <- function(rt, h = 1L, population = 1e6, detection = 1) {
     current_time <- .t[[input_size - 1L + t]]
     current_cumulative_incid <- cumulative_incid[[input_size - 1L + t]]
     current_active <- active[[input_size - 1L + t]]
-    current_susceptible <- population - current_cumulative_incid
+    if (t == 1) {
+      current_susceptible <- initial_susceptible
+    } else {
+      current_susceptible <- population - current_cumulative_incid - vac_per_day * vac_add_pct
+    }
 
     current_susceptible_pct <- current_susceptible / population
 
@@ -38,6 +42,8 @@ simulate_infections <- function(rt, h = 1L, population = 1e6, detection = 1) {
 
     new_incid <- current_rt * current_active * susceptible_adjustment
 
+    # The first simulated incidence needs to be aligned with the previously
+    # estimated one
     if (t == 1) {
       incid_correction <- next_incid - new_incid
     }
