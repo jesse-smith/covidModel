1b604fc3aa6178a01bd03ab601d62b05d64c336c
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-03-25 16:14:07 -0500

    Revert "Account for vaccinations in simulated infections"
    This reverts commit 01ce1a8cae83cc78c20c561f5abdfdce1bb12104.

--- R/simulate_infections.R
+++ R/simulate_infections.R
@@ -1,28 +1,28 @@
-simulate_infections <- function(rt, vac_init, vac_per_day = 3000, vac_init_pct = 0.25, vac_add_pct = vac_init_pct, h = 1L, population = 1e6, detection = 1/3) {
+simulate_infections <- function(rt, h = 1L, population = 1e6, detection = 1) {
 
-  # Adjust incidence for detection rate
   rt <- dplyr::mutate(rt, .incid = .incid / detection)
 
-  # Get parameters up to latest time point
   .t <- rt[[".t"]]
+
   incid <- rt[[".incid"]]
+
   cumulative_incid <- incid %>% tidyr::replace_na(0) %>% cumsum()
+
   active <- estimate_activity(rt)
 
-  # Get starting values for simulation
   input_size <- vec_size(rt)
+
   next_incid_diff <- incid[[input_size]] - incid[[input_size - 1L]]
+
   next_incid <- incid[[input_size]] + next_incid_diff
 
-  # Parameterize rt and serial interval
   current_rt <- rt[[".mean"]][[input_size]]
+
   serial_interval_params <- attr(rt, "serial_interval")
 
-  # Get initial susceptible percentage
-  initial_susceptible <- population %>%
+  initial_susceptible_pct <- population %>%
     subtract(cumulative_incid[[vec_size(cumulative_incid)]]) %>%
-    subtract(vac_init * vac_init_pct)
-  initial_susceptible_pct <- initial_susceptible / population
+    divide_by(population)
 
   for (t in seq_len(h)) {
 
@@ -30,11 +30,7 @@ simulate_infections <- function(rt, vac_init, vac_per_day = 3000, vac_init_pct =
     current_time <- .t[[input_size - 1L + t]]
     current_cumulative_incid <- cumulative_incid[[input_size - 1L + t]]
     current_active <- active[[input_size - 1L + t]]
-    if (t == 1) {
-      current_susceptible <- initial_susceptible
-    } else {
-      current_susceptible <- population - current_cumulative_incid - vac_per_day * vac_add_pct
-    }
+    current_susceptible <- population - current_cumulative_incid
 
     current_susceptible_pct <- current_susceptible / population
 
@@ -42,8 +38,6 @@ simulate_infections <- function(rt, vac_init, vac_per_day = 3000, vac_init_pct =
 
     new_incid <- current_rt * current_active * susceptible_adjustment
 
-    # The first simulated incidence needs to be aligned with the previously
-    # estimated one
     if (t == 1) {
       incid_correction <- next_incid - new_incid
     }
