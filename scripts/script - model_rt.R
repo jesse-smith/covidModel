2f6adb30ea83437957356686f5105736a708d07c
Jesse Smith <jesse.smith@shelbycountytn.gov>
2021-06-15 10:18:52 -0500

    Update figs and Rt script

--- scripts/script - model_rt.R
+++ scripts/script - model_rt.R
@@ -3,6 +3,12 @@ devtools::load_all()
 # Load yesterday's cumulative case data
 inv_data <- coviData::process_positive_people()
 
+date <- coviData::path_inv() %>%
+  fs::path_file() %>%
+  fs::path_ext_remove() %>%
+  stringr::str_extract("[0-9]{1,4}.?[0-9]{1,2}.?[0-9]{1,4}") %>%
+  lubridate::as_date()
+
 # Combine with the current report date data
 data <- dplyr::semi_join(
   coviData::load_report_date() %>% dplyr::as_tibble(),
@@ -17,11 +23,11 @@ rough_rt <- estimate_rt(data, trend = 1L, boost = FALSE)
 rt_plot <- plot_rt(rt, .rough_rt = rough_rt) %T>% {if (interactive()) show(.)}
 rt_path <- coviData::path_create(
   "C:/Users/Jesse.Smith/Documents/covidModel/figs/",
-  paste0("rt_plot", Sys.Date()-1L),
+  paste0("rt_plot", date),
   ext = "png"
 )
 
-covidReport::save_plot(rt_plot, path = rt_path)
+covidReport::save_plot(rt_plot, path = rt_path, force = TRUE)
 
 # Get current infectious activity
 
@@ -31,8 +37,9 @@ current_rt <- rt %>%
   round(digits = 2) %>%
   as.character()
 
-active <- calc_active_cases(inv_data) %>% format(big.mark = ",")
-
+active <- covidReport:::case_calc_active(inv_data) %>%
+  dplyr::filter(status == "Active") %>%
+  dplyr::pull("n")
 rt_tbl_val <- simulate_infections(rt, h = 30) %>%
   vctrs::vec_slice(i = seq(vctrs::vec_size(.) - 29, vctrs::vec_size(.), 1)) %>%
   cumsum() %>%
